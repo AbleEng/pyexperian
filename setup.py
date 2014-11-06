393f617610eacc8e8f207b74ebdf3f9748f28db1
Carlos Eduardo Rivera <cerivera@fastmail.fm>
2014-11-06 15:24:12 -0600

    added __version__ and explicit versions for required packages

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+import os
+import re
+
+v = open(os.path.join(os.path.dirname(__file__), 'pyexperian', '__init__.py'))
+VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
+v.close()
+
 try:
     from setuptools import setup
 except ImportError:
@@ -9,11 +16,13 @@ config = {
     'url': 'https://github.com/AbleEng/pyexperian',
     'download_url': 'https://github.com/AbleEng/pyexperian',
     'author_email': 'carlos@hiable.com',
-    'version': '0.5',
-    'install_requires': ['nose', 'requests>=2.2.1', 'xmltodict'],
+    'version': VERSION,
+    'install_requires': ['requests>=2.2.1', 'xmltodict>=0.9.0'],
+    'test_requires': ['nose>=1.3.4'],
     'packages': ['pyexperian', 'pyexperian.test', 'pyexperian.lib'],
     'scripts': [],
-    'name': 'pyexperian'
+    'name': 'pyexperian',
+    'keywords': 'experian net connect ecals'
 }
 
 setup(**config)
