1d734e11c3a2aa5abdc8964099ba30283d582159
Carlos Eduardo Rivera <cerivera@fastmail.fm>
2014-11-06 14:58:44 -0600

    up version to 0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'https://github.com/AbleEng/pyexperian',
     'download_url': 'https://github.com/AbleEng/pyexperian',
     'author_email': 'carlos@hiable.com',
-    'version': '0.4',
+    'version': '0.5',
     'install_requires': ['nose', 'requests', 'xmltodict'],
     'packages': ['pyexperian', 'pyexperian.test', 'pyexperian.lib'],
     'scripts': [],
