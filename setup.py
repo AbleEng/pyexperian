1289601f14cd7c679c0cce2f2cd349c771346102
Carlos Eduardo Rivera <cerivera@fastmail.fm>
2014-12-10 11:51:46 -0600

    revert versions to old way

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
-import os
-import re
-
-v = open(os.path.join(os.path.dirname(__file__), 'pyexperian', '__init__.py'))
-VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
-v.close()
-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 config = {
+    'version': '0.12.7',
     'description': 'Python wrapper for Experian\'s Net Connect API',
     'author': 'Carlos Eduardo Rivera',
     'url': 'https://github.com/AbleEng/pyexperian',
     'download_url': 'https://github.com/AbleEng/pyexperian',
     'author_email': 'carlos@hiable.com',
-    'version': VERSION,
     'install_requires': ['requests>=2.2.1', 'xmltodict>=0.9.0'],
     'tests_require': ['nose>=1.3.4'],
     'packages': ['pyexperian', 'pyexperian.test', 'pyexperian.lib'],
